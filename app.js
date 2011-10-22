d422b8e9ddc64dad94d3df16eef484bf70f24b52
Scott Ballantyne <ussballantyne@gmail.com>
2011-10-22 19:53:46 +0800

    trying to make it work

--- app.js
+++ app.js
@@ -4,28 +4,39 @@ var express = require('express'),
   , io = io.listen(app)
   , nicknames = {};
 
+io.enable('browser client minification');  // send minified client
+io.enable('browser client etag');          // apply etag caching logic based on version number
+io.enable('browser client gzip');          // gzip the file
+io.set('log level', 1);                    // reduce logging
+io.set('transports', [                     // enable all transports (optional if you want flashsocket)
+  'xhr-polling'
+]);
+
+
 io.sockets.on('connection', function (socket) {
-  socket.on('user message', function (msg) {
-    socket.broadcast.emit('user message', socket.username, msg);
+  console.log('socket connection')
+  // socket.on('user message', function (msg) {
+  //   socket.broadcast.emit('user message', 'test', msg);
+  // });
+  // 
+  // socket.on('news', function (nick, fn) {
+  //   if (nicknames[nick]) {
+  //     fn(true);
+  //   } else {
+  //     fn(false);
+  //     nicknames[nick] = socket.nickname = nick;
+  //     socket.broadcast.emit('announcement', nick + ' connected');
+  //     io.sockets.emit('news', nicknames);
+  //   }
+  // });
+  // 
+  socket.on('new_user_message', function (data) {
+    console.log(data)
+    socket.broadcast.emit('new_user_message', 'test');
   });
-
-  socket.on('username', function (nick, fn) {
-    if (nicknames[nick]) {
-      fn(true);
-    } else {
-      fn(false);
-      nicknames[nick] = socket.nickname = nick;
-      socket.broadcast.emit('announcement', nick + ' connected');
-      io.sockets.emit('nicknames', nicknames);
-    }
-  });
-
+  
   socket.on('disconnect', function () {
-    if (!socket.username) return;
-
-    delete nicknames[socket.nickname];
-    socket.broadcast.emit('announcement', socket.username + ' disconnected');
-    socket.broadcast.emit('nicknames', nicknames);
+    console.log('disconnect')
   });
 });
 
