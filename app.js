91cebd124523c196a0d8ebd47940f77c4bd17126
Scott Ballantyne <ussballantyne@gmail.com>
2012-02-29 13:18:43 +0800

    another try

--- app.js
+++ app.js
@@ -91,7 +91,6 @@ io.sockets.on('connection', function (socket) {
 if (!module.parent) {
   if (NODE_ENV !== 'development') {
     cluster(app)
-      .use(cluster.logger('logs'))
       .use(cluster.stats())
       .use(cluster.pidfiles('pids'))
       .use(cluster.cli())
